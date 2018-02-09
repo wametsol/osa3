4b536e9c147ff7a0ffd866ad7ca05a8f13b552fc
wametsol <waltteri.metsola@gmail.com>
2018-02-09 15:14:41 +0200

    3.4
    Lisätty app.delete, ja testattu Postmanilla.

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ app.get('/info', (req, res) =>{
 
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
 app.get('/api/persons/:id', (req, res) =>{
     const id = Number(req.params.id)
     const person = persons.find(person => person.id === id)
